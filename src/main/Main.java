ad6f28e82a228d9596d77ce5c90267c43a74d1c9
stban2703 <s.t.ban1401@gmail.com>
2020-03-11 09:28:08 -0500

    Primer commit

---
+++ src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.io.IOException;
+
+import comm.ComunicacionTCP;
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("main.Main");
+	}
+
+	ComunicacionTCP comm;
+
+	public void settings() {
+		size(500, 500);
+	}
+
+	public void setup() {
+		comm = new ComunicacionTCP();
+		comm.esperarConexion();
+
+	}
+
+	public void draw() {
+		background(255);
+		
+	}
+	
+	public void mousePressed() {
+		//comm.mandarMensaje("NO");
+	}
+
+}
